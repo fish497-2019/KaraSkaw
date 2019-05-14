b68f4a57b4ad3c18572b79f22a6b109415d6c2be
skawkara6 <skawkara6@gmail.com>
2019-05-14 11:07:13 -0700

    Change size cut off for new column

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,4 +1,5 @@
 fish_data = read.csv("Gaeta_etal_CLC_data.csv")
+#Creating a new categorical size column
 library(dplyr)
 fish_data_cat = fish_data %>%
-  mutate(length_cat = ifelse(length > 200, "big", "small"))
+  mutate(length_cat = ifelse(length > 300, "big", "small"))
