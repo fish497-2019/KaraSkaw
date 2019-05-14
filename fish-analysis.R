44157b0db82ba7a9c7fa9e9e98848cc28156271f
skawkara6 <skawkara6@gmail.com>
2019-05-14 11:25:01 -0700

    Removed fish with a scale length less than 1mm

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,5 +1,8 @@
-fish_data = read.csv("Gaeta_etal_CLC_data.csv")
+fish_data <-  read.csv("Gaeta_etal_CLC_data.csv")
 #Creating a new categorical size column
 library(dplyr)
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
+#Removing fish with scale length less then 1mm
+select(fish_data_cat, scalelength) %>%
+  filter(scalelength > 1)
