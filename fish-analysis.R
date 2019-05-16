d1ddb5cd256d1c47de5d7b3161cb180a59555c99
skawkara6 <skawkara6@gmail.com>
2019-05-16 10:31:03 -0700

    Code and image for histogram of scale length categorized by size

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,6 +1,6 @@
 #Create an object called fish_data for the raw data
-fish_data <-  read.csv("Gaeta_etal_CLC_data.csv")
 
+fish_data <-  read.csv("C:/Users/skawk/OneDrive/Documents/Git_Hub/KaraSkaw/Gaeta_etal_CLC_data_1.csv.csv")
 #Creating a new categorical size column
 library(dplyr)
 fish_data_cat = fish_data %>%
@@ -18,4 +18,14 @@ library(ggplot2)
 ggplot(data = fish_data_cat)+
   geom_point(mapping = aes(x=length, y=scalelength, color = lakeid))+
   labs(x = "Length of fish's body", y = "Scale length")
-  
+
+#Pulling code from GitHub
+ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
+  geom_histogram()
+
+#Modify code & set the bins argument to 80
+ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
+  geom_histogram(bins=80)
+
+#Saving plot
+ggsave("scale_hist_by_length.jpg")
